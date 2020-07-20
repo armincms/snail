ed34d0207fdc5734f8a31a64fe916f37d8848695
اسماعیل زارع <zarehesmaiel@gmail.com>
2020-07-20 11:33:23 +0430

    Creates the `HasMany` property

---
+++ src/Properties/HasMany.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Armincms\Snail\Properties;
+   
+use Armincms\Snail\Contracts\ListableProperty;
+
+class HasMany extends Relation implements ListableProperty
+{        
+    /**
+     * Resolve the field's value.
+     *
+     * @param  mixed  $resource
+     * @param  string|null  $attribute
+     * @return void
+     */
+    public function resolve($resource, $attribute = null)
+    {
+        $value = null;
+
+        if ($resource->relationLoaded($this->attribute)) {
+            $value = $resource->getRelation($this->attribute);
+        }
+
+        if (! $value) {
+            $value = $resource->{$this->attribute}()->withoutGlobalScopes()->getResults();
+        } 
+
+        if ($value) { 
+            $this->value = $value->mapInto($this->resourceClass)->map(function($schema) {
+                return $schema->serializeForDisplay($this->resolvePropertiesForDisplay($schema));
+            });
+        }
+    }
+}
